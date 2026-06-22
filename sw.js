1730e18db7ea85317e933ac6ef01e5bfe6990678
Rauf <raufhossain548@gmail.com>
2026-06-23 04:08:27 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE="break-record-v1";
+
+
+self.addEventListener(
+"install",
+e=>{
+
+e.waitUntil(
+
+caches.open(CACHE)
+.then(c=>c.addAll([
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json"
+]))
+
+);
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+e=>{
+
+e.respondWith(
+
+caches.match(e.request)
+.then(r=>r || fetch(e.request))
+
+);
+
+});
